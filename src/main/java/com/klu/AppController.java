67deacbc46f6159d624d9383c8c406f4b478f709
MAYAKUNTLALAKSHMIVIJAYA <mayakuntlalakshmi216@gmail.com>
2025-08-09 17:12:24 +0530

    Update AppController.java

--- src/main/java/com/klu/AppController.java
+++ src/main/java/com/klu/AppController.java
@@ -8,35 +8,43 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-// import org.springframework.web.bind.annotation.DeleteMapping;
-// import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @CrossOrigin
 public class AppController {
-	@Autowired
-	Service s;
+    
+    @Autowired
+    Service s;
 
-	@PostMapping("/insert")
-	public String insert(@RequestBody Product p) {
-		return s.insertData(p);
-	}
+    // Root landing page
+    @GetMapping("/")
+    public String home() {
+        return "✅ SpringApp1 is running! Available endpoints: "
+                + "[/insert - POST], "
+                + "[/display - GET], "
+                + "[/update - PUT]";
+    }
 
-	@GetMapping("/display")
-	public List<Product> display() {
-		return s.displayData();
-	}
+    @PostMapping("/insert")
+    public String insert(@RequestBody Product p) {
+        return s.insertData(p);
+    }
 
-	@PutMapping("/update")
-	public String update(@RequestBody Product p) {
-		return s.updateData(p);
-	}
+    @GetMapping("/display")
+    public List<Product> display() {
+        return s.displayData();
+    }
 
-	/* 
-	@DeleteMapping("/delete/{id}")
-	public String delete(@PathVariable int id) {
-		return s.deleteData(id);
-	}
-	*/
+    @PutMapping("/update")
+    public String update(@RequestBody Product p) {
+        return s.updateData(p);
+    }
+
+    /*
+    @DeleteMapping("/delete/{id}")
+    public String delete(@PathVariable int id) {
+        return s.deleteData(id);
+    }
+    */
 }
